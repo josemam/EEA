ca9d21066e6d0632f0312d893e28a7c077c8a5f6
josemam <josemam@users.noreply.github.com>
2015-10-05 19:42:59 +0200

    Added inv

--- eea.hs
+++ eea.hs
@@ -5,3 +5,9 @@ eea :: Integer -> Integer -> [Integer]
 eea a 0 = [a, 1, 0]
 eea a b = let s = ((eea b (a-(quot a b)*b)) !! 2)
           in sort (zipWith (-) (eea b (a-(quot a b)*b)) [0, (quot a b)*s, 0])
+
+inv :: Integer -> Integer -> Integer
+inv a b
+          | eab !! 0 == 1 = mod (eab !! 1) b
+          | otherwise = 0
+          where eab = eea a b
